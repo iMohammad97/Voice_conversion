c97a9a669a832390b53072cf4f86a285368414de
mohammad <mohammadm97i@gmail.com>
2019-07-12 23:22:14 +0430

    Done

---
+++ VoiceTransformer/AMFM_decompy-1.0.8.1/bin/AMFM_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+Script to test the AMFM_decompy package.
+
+Version 1.0.8.1
+09/Jul/2018 Bernardo J.B. Schmitt - bernardo.jb.schmitt@gmail.com
+"""
+import amfm_decompy
+import amfm_decompy.pYAAPT as pyaapt
+import amfm_decompy.pyQHM as pyqhm
+import amfm_decompy.basic_tools as basic
+import os.path
+
+# Declare the variables.
+file_name = os.path.dirname(amfm_decompy.__file__)+os.sep+"sample.wav"
+window_duration = 0.015   # in seconds
+nharm_max = 25
+SNR = float('Inf')
+
+# Create the signal object.
+signal = basic.SignalObj(file_name)
+
+# Create the window object.
+window = pyqhm.SampleWindow(window_duration, signal.fs)
+
+# Create the pitch object and calculate its attributes.
+pitch = pyaapt.yaapt(signal)
+
+# Set the number of modulated components.
+signal.set_nharm(pitch.values, nharm_max)
+
+# Check if gaussian noise has to be added.
+if SNR != float('Inf'):
+    signal.noiser(pitch.values, SNR)
+
+# Perform the QHM extraction.
+QHM = pyqhm.qhm(signal, pitch, window, 0.001, N_iter = 3, phase_tech = 'phase')
+
+print ("QHM SRER: {}".format(QHM.SRER))
+
+# Perform the aQHM extraction.
+aQHM = pyqhm.aqhm(signal, QHM, pitch, window, 0.001, N_iter = 3, N_runs = 2,
+            phase_tech = 'phase')
+
+print ("aQHM SRER: {}".format(aQHM.SRER))
+
+# Perform the eaQHM extraction.
+eaQHM = pyqhm.eaqhm(signal, aQHM, pitch, window, 0.001, N_iter=3, N_runs=2,
+              phase_tech = 'phase')
+
+print ("eaQHM SRER: {}".format(eaQHM.SRER))
+
---
+++ VoiceTransformer/AMFM_decompy-1.0.8.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import io
+from setuptools import setup
+
+try:
+    long_description = io.open('README.txt',encoding='utf8').read()
+except:
+    long_description ='Package containing the tools necessary for decomposing a \
+            speech signal into its modulated components, aka AM-FM decomposition.'
+
+setup(
+    name = 'AMFM_decompy',
+    version = '1.0.8.1',
+    author = 'Bernardo J.B. Schmitt',
+    author_email = 'bernardo.jb.schmitt@gmail.com',
+    packages = ['amfm_decompy'],
+    scripts = ['bin/AMFM_test.py'],
+    package_data = {'amfm_decompy': ['*.wav']},
+    install_requires = ['numpy', 'scipy',],
+    url = 'https://github.com/bjbschmitt/AMFM_decompy/',
+    license = 'LICENSE.txt',
+    description = 'Package containing the tools necessary for decomposing a \
+speech signal into its modulated components, aka AM-FM decomposition.',
+    long_description = long_description,
+    classifiers = [
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.5',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+                'Topic :: Scientific/Engineering :: Information Analysis',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+                ],
+    keywords = 'Python, speech, pitch, QHM, YAAPT, modulated components, \
+                AM-FM decomposition',
+    zip_safe = False,
+    include_package_data = True,
+)
